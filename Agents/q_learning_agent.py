762db79530895adb4da646e947499f0e67b73342
s-mawjee <21179950+s-mawjee@users.noreply.github.com>
2018-03-30 14:45:21 +0200

    Pulled out loop from agent into trainer.

--- Agents/q_learning_agent.py
+++ Agents/q_learning_agent.py
@@ -1,9 +1,8 @@
-from Agents.agent import Agent
-import itertools
-import numpy as np
-import sys
-
 from collections import defaultdict
+
+import numpy as np
+
+from Agents.agent import Agent
 
 
 class QLearningAgent(Agent):
@@ -18,55 +17,60 @@ class QLearningAgent(Agent):
         # A nested dictionary that maps state -> (action -> action-value).
         self.Q = defaultdict(lambda: np.zeros(self.env.action_space.n))
 
-    def make_epsilon_greedy_policy(self):
+    def epsilon_greedy_policy(self, observation):
+        action = np.ones(self.env.action_space.n, dtype=float) * self.epsilon / self.env.action_space.n
+        best_action = np.argmax(self.Q[observation])
+        action[best_action] += (1.0 - self.epsilon)
+        return action
 
-        def policy_fn(observation):
-            A = np.ones(self.env.action_space.n, dtype=float) * \
-                self.epsilon / self.env.action_space.n
-            best_action = np.argmax(self.Q[observation])
-            A[best_action] += (1.0 - self.epsilon)
-            return A
-        return policy_fn
+    def get_action(self, state):
+        action_probs = self.epsilon_greedy_policy(state)
+        action = np.random.choice(
+            np.arange(len(action_probs)), p=action_probs)
+        return action
 
-    def learn(self):
+    def update(self, state, action, reward, next_state):
+        best_next_action = np.argmax(self.Q[next_state])
+        td_target = reward + self.discount_factor * self.Q[next_state][best_next_action]
+        td_delta = td_target - self.Q[state][action]
+        self.Q[state][action] += self.alpha * td_delta
 
-        # The policy we're following
-        policy = self.make_epsilon_greedy_policy()
-
-        for i_episode in range(self.num_episodes):
-            # Print out which episode we're on, useful for debugging.
-            if (i_episode + 1) % 100 == 0:
-                print("\rEpisode {}/{}".format(i_episode +
-                                               1, self.num_episodes))
-                sys.stdout.flush()
-
-            # Reset the environment and pick the first action
-            state = self.env.reset()
-
-            # One step in the environment
-            # total_reward = 0.0
-            for t in itertools.count():
-
-                # Take a step
-                action_probs = policy(state)
-                action = np.random.choice(
-                    np.arange(len(action_probs)), p=action_probs)
-                next_state, reward, done, _ = self.env.step(action)
-
-                # Update statistics
-                self.episode_rewards[i_episode] += reward
-                self.episode_lengths[i_episode] = t
-
-                # TD Update
-                best_next_action = np.argmax(self.Q[next_state])
-                td_target = reward + self.discount_factor * \
-                    self.Q[next_state][best_next_action]
-                td_delta = td_target - self.Q[state][action]
-                self.Q[state][action] += self.alpha * td_delta
-
-                if done:
-                    break
-
-                state = next_state
-
-        return self.Q  # , stats
+    # def learn(self):
+    #
+    #     for i_episode in range(self.num_episodes):
+    #         # Print out which episode we're on, useful for debugging.
+    #         if (i_episode + 1) % 100 == 0:
+    #             print("\rEpisode {}/{}".format(i_episode +
+    #                                            1, self.num_episodes))
+    #             sys.stdout.flush()
+    #
+    #         # Reset the environment and pick the first action
+    #         state = self.env.reset()
+    #
+    #         # One step in the environment
+    #         # total_reward = 0.0
+    #         for t in itertools.count():
+    #
+    #             # Take a step
+    #             action_probs = self.epsilon_greedy_policy(state)
+    #             action = np.random.choice(
+    #                 np.arange(len(action_probs)), p=action_probs)
+    #             next_state, reward, done, _ = self.env.step(action)
+    #
+    #             # Update statistics
+    #             self.episode_rewards[i_episode] += reward
+    #             self.episode_lengths[i_episode] = t
+    #
+    #             # TD Update
+    #             best_next_action = np.argmax(self.Q[next_state])
+    #             td_target = reward + self.discount_factor * \
+    #                 self.Q[next_state][best_next_action]
+    #             td_delta = td_target - self.Q[state][action]
+    #             self.Q[state][action] += self.alpha * td_delta
+    #
+    #             if done:
+    #                 break
+    #
+    #             state = next_state
+    #
+    #     return self.Q  # , stats
--- trainer.py
+++ trainer.py
@@ -1,8 +1,11 @@
+import itertools
+import sys
+
 from Agents.q_learning_agent import QLearningAgent
 from Environments.gridworld import GridworldEnv
 
 if __name__ == '__main__':
-    num_episodes = 100
+    num_episodes = 200
     discount_factor = 1.0
     alpha = 0.5
     epsilon = 0.1
@@ -11,6 +14,37 @@ if __name__ == '__main__':
     agent = QLearningAgent(env, num_episodes,  discount_factor, alpha, epsilon)
 
     print('START - ' + agent.get_name())
-    agent.learn()
-    agent.plot_episode_stats()
+
+    for i_episode in range(num_episodes):
+        # Print out which episode we're on, useful for debugging.
+        if (i_episode + 1) % 100 == 0:
+            print("\rEpisode {}/{}".format(i_episode +
+                                           1, num_episodes))
+            sys.stdout.flush()
+
+        # Reset the environment and pick the first action
+        state = env.reset()
+
+        # One step in the environment
+        # total_reward = 0.0
+        for t in itertools.count():
+
+            # Take a step
+            action = agent.get_action(state)
+            next_state, reward, done, _ = env.step(action)
+
+            # Update statistics
+            agent.episode_rewards[i_episode] += reward
+            agent.episode_lengths[i_episode] = t
+
+            # TD Update
+            agent.update(state, action, reward, next_state)
+
+            if done:
+                break
+
+            state = next_state
+
+    # agent.plot_episode_stats()
     print('END - ' + agent.get_name())
+    print(agent.Q)
